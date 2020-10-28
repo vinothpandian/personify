abb6c07be1d1c618c730a825dd9e3b93d88ff21d
Vinoth Pandian <vinothpandian@live.com>
2020-10-28 22:56:42 +0100

    add persona avatars

--- src/app/persona-panel/persona-panel.component.ts
+++ src/app/persona-panel/persona-panel.component.ts
@@ -6,6 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona-panel.component.scss'],
 })
 export class PersonaPanelComponent implements OnInit {
+  personaImages = [
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+    'assets/images/persona_1.png',
+    'assets/images/persona_2.png',
+    'assets/images/persona_3.png',
+    'assets/images/persona_4.png',
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
