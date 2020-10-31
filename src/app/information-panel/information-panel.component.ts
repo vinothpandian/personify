cff5e69aa8931516061f23ebe00f0a2e9b467a22
Vinoth Pandian <vinothpandian@live.com>
2020-10-31 20:40:06 +0100

    fix guidelines

--- src/app/information-panel/information-panel.component.ts
+++ src/app/information-panel/information-panel.component.ts
@@ -1,94 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Neo4jService } from '../core/services/neo4j.service';
-import { Persona } from '../models';
+import { Guideline, Persona } from '../models';
 
 @Component({
   selector: 'app-information-panel',
   templateUrl: './information-panel.component.html',
   styleUrls: ['./information-panel.component.scss'],
 })
-export class InformationPanelComponent implements OnInit {
+export class InformationPanelComponent implements OnInit, OnDestroy {
   persona: Persona = {} as Persona;
 
-  guidelines = [
-    {
-      image: 'assets/images/on_focus.png',
-      title: 'On Focus',
-      subtitle: 'WCAG 2.1 Predictable',
-      description:
-        'Any component that is able to trigger an event when it receives focus must not change the cont ext.',
-    },
-    {
-      image: 'assets/images/error.png',
-      title: 'Error Suggestion',
-      subtitle: 'WCAG 2.1 Input Assistance',
-      description:
-        'If an input error is detected (via client-side or server-side validation), suggestions are provided for fixing the input.',
-    },
-
-    {
-      image: 'assets/images/on_focus.png',
-      title: 'On Focus',
-      subtitle: 'WCAG 2.1 Predictable',
-      description:
-        'Any component that is able to trigger an event when it receives focus must not change the cont ext.',
-    },
-    {
-      image: 'assets/images/error.png',
-      title: 'Error Suggestion',
-      subtitle: 'WCAG 2.1 Input Assistance',
-      description:
-        'If an input error is detected (via client-side or server-side validation), suggestions are provided for fixing the input.',
-    },
-
-    {
-      image: 'assets/images/on_focus.png',
-      title: 'On Focus',
-      subtitle: 'WCAG 2.1 Predictable',
-      description:
-        'Any component that is able to trigger an event when it receives focus must not change the cont ext.',
-    },
-    {
-      image: 'assets/images/error.png',
-      title: 'Error Suggestion',
-      subtitle: 'WCAG 2.1 Input Assistance',
-      description:
-        'If an input error is detected (via client-side or server-side validation), suggestions are provided for fixing the input.',
-    },
-
-    {
-      image: 'assets/images/on_focus.png',
-      title: 'On Focus',
-      subtitle: 'WCAG 2.1 Predictable',
-      description:
-        'Any component that is able to trigger an event when it receives focus must not change the cont ext.',
-    },
-    {
-      image: 'assets/images/error.png',
-      title: 'Error Suggestion',
-      subtitle: 'WCAG 2.1 Input Assistance',
-      description:
-        'If an input error is detected (via client-side or server-side validation), suggestions are provided for fixing the input.',
-    },
-
-    {
-      image: 'assets/images/on_focus.png',
-      title: 'On Focus',
-      subtitle: 'WCAG 2.1 Predictable',
-      description:
-        'Any component that is able to trigger an event when it receives focus must not change the cont ext.',
-    },
-    {
-      image: 'assets/images/error.png',
-      title: 'Error Suggestion',
-      subtitle: 'WCAG 2.1 Input Assistance',
-      description:
-        'If an input error is detected (via client-side or server-side validation), suggestions are provided for fixing the input.',
-    },
-  ];
+  guidelines: Guideline[] = [];
 
   personaSubscription: Subscription;
+  guidelineSubscription: Subscription;
 
   constructor(private neo4jService: Neo4jService) {}
 
@@ -98,5 +24,16 @@ export class InformationPanelComponent implements OnInit {
         this.persona = persona;
       }
     );
+
+    this.guidelineSubscription = this.neo4jService.guidelines$.subscribe(
+      (guidelines) => {
+        this.guidelines = guidelines;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.personaSubscription.unsubscribe();
+    this.guidelineSubscription.unsubscribe();
   }
 }
