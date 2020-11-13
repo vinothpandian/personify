378476ddcea497affe2d6e7238444503f2bad885
Vinoth Pandian <vinothpandian@live.com>
2020-11-13 21:01:14 +0100

    Remove Dead Link on the Personify Logo (Text) #5
    * fix dropdown in search not closing

--- src/app/graph/graph.module.ts
+++ src/app/graph/graph.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { GraphComponent } from './graph.component';
 
 @NgModule({
   declarations: [GraphComponent],
-  imports: [CommonModule, NgSelectModule],
+  imports: [CommonModule, NgSelectModule, FormsModule],
   exports: [GraphComponent],
 })
 export class GraphModule {}
